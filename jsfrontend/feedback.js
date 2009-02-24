6cc054b07cad183c50b9a8483f48e0124770a5ec
James Duncan <james.t.duncan@gmail.com>
2009-02-24 10:35:33 -0800

    Added prototype.js (1.6) to jsfrontend, and did some sample js work

--- jsfrontend/feedback.js
+++ jsfrontend/feedback.js
@@ -1,18 +1,29 @@
 var fb = {
 	sampleProperty:"Yay!  Code!",
+	// Will hold generated DOM elements
+	el:{},
+	// Various environment variables
 	env:{
-		"init":false
+		// A boolean indicating if init() had been run yet
+		"init":false,
+		// A reference to the main <body> tag
+		"body":$$('body')[0]
 	},
 	init:function(){
 		if (fb.env.init) {
-			console.log("Already initialized!");
 			return;
 		}
+		fb.el.toggleButton = new Element('div',{'style':'background:#000000;color:#FFFFFF;position:fixed;top:0px;left:0px;'}).update("C");
+		fb.el.main = new Element('div',{'style':'background:#000000;color:#FFFFFF;position:fixed;top:0px;right:0px;display:none;'}).update("hello");
+		fb.env.body.insert(fb.el.toggleButton);
+		fb.env.body.insert(fb.el.main);
+		fb.el.toggleButton.observe('click',function(){fb.el.main.toggle();});
+
 		fb.env.init = true;
-		console.log("Initialized!");
 		return;
 	}
 };
+
 if (fb.init) {
 	fb.init();
 }
