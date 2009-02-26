17b6a847c307e67aca1ec4661a2534f247749339
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-02-26 14:18:11 -0800

    fixing SitesControllerTest broken in last commit due to updated fixtures

--- coreapp/test/functional/sites_controller_test.rb
+++ coreapp/test/functional/sites_controller_test.rb
@@ -32,30 +32,30 @@ class SitesControllerTest < ActionController::TestCase
 
 
   test "should show site" do
-    get :show, :id => sites(:one).id
+    get :show, :id => sites(:linkedin).id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => sites(:one).id
+    get :edit, :id => sites(:linkedin).id
     assert_response :success
   end
 
   test "should update site" do
-    put :update, :id => sites(:one).id, :site => valid_options_for_site
+    put :update, :id => sites(:linkedin).id, :site => valid_options_for_site
     assert_redirected_to site_path(assigns(:site))
   end
 
   test "should go back to edit if updating an site with invalid parameters" do
     unless valid_options_for_site.keys.empty?
-      put :update, :id => sites(:one).id, :site => { valid_options_for_site.keys.first => nil }
+      put :update, :id => sites(:linkedin).id, :site => { valid_options_for_site.keys.first => nil }
       assert_template "edit"
     end
   end
 
   test "should destroy site" do
     assert_difference('Site.count', -1) do
-      delete :destroy, :id => sites(:one).id
+      delete :destroy, :id => sites(:linkedin).id
     end
 
     assert_redirected_to sites_path
