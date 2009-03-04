e13993385ee7bcfd94be2dbfce693959403506cb
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-03-04 00:05:05 -0800

    [Story462030 state:finished] fixed improper nesting in CommentsController#create transaction

--- coreapp/test/functional/commenters_controller_test.rb
+++ coreapp/test/functional/commenters_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class CommentersControllerTest < ActionController::TestCase
   
+  self.use_transactional_fixtures = false
+  
   def setup
     @page = pages(:fb_profile)
   end
@@ -18,14 +20,37 @@ class CommentersControllerTest < ActionController::TestCase
   end
 
   test "should create commenters" do
-    assert_difference 'Commenter.count', 3 do
-      assert_difference "Invite.count", 3 do
-        emails = ["avk@berkeley.edu", "hlhu@berkeley.edu", "mkocher@berkeley.edu"]
+    emails = ["avk@berkeley.edu", "hlhu@berkeley.edu", "mkocher@berkeley.edu"]
+    assert_difference 'Commenter.count', emails.size do
+      assert_difference "Invite.count", emails.size do
         post :create, :emails => emails.join(', '), :page_id => @page.id
       end
     end
 
     assert_redirected_to page_commenters_path(@page)
+  end
+
+  test "should render page when new fails" do
+    emails = ['bullshit', '@.c', '9238740923874092837049823']
+    assert_no_difference('Commenter.count') do
+      assert_no_difference "Invite.count" do
+        post :create, :emails => emails.join(', '), :page_id => @page.id
+      end
+    end
+  
+    assert_redirected_to @page
+  end
+  
+  test "should not invite duplicate commenters" do
+    good_emails = ["avk@berkeley.edu", "hlhu@berkeley.edu", "mkocher@berkeley.edu"]
+    mixed_emails = [good_emails.first, "json@berkeley.edu"]
+    num_new = (good_emails | mixed_emails).size
+    assert_difference "Commenter.count", num_new do
+      assert_difference "Invite.count", num_new do
+        post :create, :emails => good_emails.join(', '), :page_id => @page.id
+        post :create, :emails => mixed_emails.join(', '), :page_id => @page.id
+      end
+    end
   end
 
   test "should show commenter" do
@@ -43,27 +68,16 @@ class CommentersControllerTest < ActionController::TestCase
     assert_redirected_to page_commenter_path(@page, assigns(:commenter))
   end
 
+  test "should render update when update commenter fails" do
+	  put :update, :id => commenters(:one).id, :commenter => invalid_options_for_commenters, :page_id => @page.id
+	  assert_template "edit"
+  end
+
   test "should destroy commenter" do
     assert_difference('Commenter.count', -1) do
       delete :destroy, :id => commenters(:one).id, :page_id => @page.id
     end
 
     assert_redirected_to page_commenters_path(@page)
-  end
-
-  test "should render page when new fails" do
-    assert_no_difference('Commenter.count') do
-      assert_no_difference "Invite.count" do
-        emails = ['bullshit', '@.c', '9238740923874092837049823']
-        post :create, :emails => emails.join(', '), :page_id => @page.id
-      end
-    end
-  
-    assert_redirected_to @page
-  end
-
-  test "should render update when update commenter fails" do
-	  put :update, :id => commenters(:one).id, :commenter => invalid_options_for_commenters, :page_id => @page.id
-	  assert_template "edit"
   end
 end
