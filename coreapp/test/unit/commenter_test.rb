adb30f9015b2f535aa74715e988acd7a693c6ae9
Hannah Hu <hlhu@berkeley.edu>
2009-03-07 17:47:48 -0800

    Added has_many :feedbacks, :dependent => :destroy to Commenter model and wrote tests for that. Also attempting to delete .svn folders

--- coreapp/test/unit/commenter_test.rb
+++ coreapp/test/unit/commenter_test.rb
@@ -40,4 +40,48 @@ class CommenterTest < ActiveSupport::TestCase
     end
   end
 
+  test "should, when destroying a commenter, delete all feedback associated with it" do
+    commenter = nil
+    
+    assert_difference "Commenter.count" do
+      commenter = create_commenter
+    end
+    
+    comments = %w(cool nifty awesome!)
+    assert_difference "Feedback.count", comments.size do
+      comments.each do |comment|
+        commenter.feedbacks << create_feedback(:content => comment, :commenter_id => commenter.id)
+      end
+      commenter.save
+    end
+    
+    assert_difference "Commenter.count", -1 do
+      assert_difference "Feedback.count", -(comments.size) do
+        commenter.destroy
+      end
+    end
+  end
+  
+  test "should, when destroying a commenter, delete all invites associated with it" do
+    # commenter = nil
+    # 
+    # assert_difference "Commenter.count" do
+    #   commenter = create_commenter
+    # end
+    # 
+    # invites = [a bunch of invites]
+    # assert_difference "Invite.count", invites.size do
+    #   invites.each do |invite|
+    #     commenter.invites << create_invite(:commenter_id => commenter.id)
+    #   end
+    #   commenter.save
+    # end
+    # 
+    # assert_difference "Commenter.count", -1 do
+    #   assert_difference "Invite.count", -(invites.size) do
+    #     commenter.destroy
+    #   end
+    # end
+  end
+  
 end
