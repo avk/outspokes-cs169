0dec9b742c90fa45485d10a116e336ff4d80ca3d
Alex Pretzlav <alex@turnlav.net>
2009-03-22 17:16:00 -0700

    Public feedbacks tests

--- coreapp/test/unit/public_feedback_test.rb
+++ coreapp/test/unit/public_feedback_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class PublicFeedbackTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "can create public feedbacks without an account" do
+    assert_difference "PublicFeedback.count" do
+      f = PublicFeedback.create(:page => Page.find(:first), :name => "Joe", :content => "lulz", :target => "html")
+      assert_valid f
+    end
   end
+  
+  test "can't create a public comment without a name" do
+    assert_no_difference "PublicFeedback.count" do
+      f = PublicFeedback.create(:page => Page.find(:first), :content => "lulz", :target => "html")
+      assert !f.valid?
+    end
+  end
+  
 end
