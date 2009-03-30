01a6ed097a2043cce1e3073170be49390ae38348
sanxac <sanja9@gmail.com>
2009-03-29 19:48:01 -0700

    added tagging to comments via the acts_as_taggable_on_steroids plugin

--- coreapp/app/controllers/feedbacks_controller.rb
+++ coreapp/app/controllers/feedbacks_controller.rb
@@ -93,6 +93,23 @@ class FeedbacksController < ApplicationController
     end
   end
 
+  # POST /pages/1/feedbacks/1/add_tag
+  def add_tag
+    @feedback = Feedback.find(params[:id])
+	  @feedback.tag_list.add(params[:tag_list].gsub(" ", "_")) 
+	  @feedback.save
+    flash[:notice] = 'Tag Added'
+    redirect_to page_path(params[:page_id])
+  end
+
+  def delete_tag
+    @feedback = Feedback.find(params[:id])
+    @feedback.tag_list.remove(params[:tag])
+    @feedback.save!
+    flash[:notice] = 'Tag Removed'
+    redirect_to page_path(params[:page_id])
+  end
+
 protected
 
   def same_domain?(url1, url2)
