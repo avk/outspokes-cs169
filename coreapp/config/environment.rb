8090d37cc356c7ee19d0d127c599f5f5318e49eb
Andrew Bernstein <abernstein@berkeley.edu>
2009-04-16 22:27:05 -0700

    Actionmailer settings for matthew's dreamhost smtp account

--- coreapp/config/environment.rb
+++ coreapp/config/environment.rb
@@ -76,4 +76,18 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
+  # Email config settings for Matthew's dreamhost email account
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address          => 'mail.reallycooldomainname.com',
+    :port             => 587,
+    :domain           => 'reallycooldomainname.com',
+    :authentication   => :login,
+    :user_name        => 'outspokes@reallycooldomainname.com',
+    :password         => 'inktomi2009'
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_charset = 'utf-8'
+
 end
