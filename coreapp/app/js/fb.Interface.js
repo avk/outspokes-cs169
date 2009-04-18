19bd3266dafba2a7f6e77f940ec1ac512be39750
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-04-18 14:06:48 -0700

    initial admin panel interface (sans authentication and iframe)

--- coreapp/app/js/fb.Interface.js
+++ coreapp/app/js/fb.Interface.js
@@ -6,9 +6,48 @@
    * @constructor
    */
   fb.Interface = function() {
-    fb.assert_false(fb.Interface.instantiated, "Can not create more than one instance of the interface.")
+    fb.assert_false(fb.Interface.instantiated, "Can not create more than one instance of the interface.");
     
-    this.main_window = $('<div id="outspokes"></div>').css({
+    this.dom = {
+      widget  : 'outspokes',
+      admin   : {
+        panel   : 'outspokes_admin_panel',
+        open    : 'open_admin_panel',
+        close   : 'close_admin_panel',
+        overlay : 'outspokes_overlay',
+      },
+    }
+    
+    this.admin_panel = {
+      dom   : this.dom,
+      build : function(widget) { // TODO: only show for admins
+        // the actual panel
+        var admin_panel = $('<div></div>').attr('id',this.dom.admin.panel);
+        var close_link = $("<a href='#'>x</a>").attr('id',this.dom.admin.close);
+        close_link.click(this.hide);
+        admin_panel.append(close_link);
+        admin_panel.append("<h1>I'm the ADMIN, bitches!</h1>"); // TODO: replace with coreapp iframe
+        admin_panel.appendTo($('body'));
+        
+        // the background overlay
+        $('<div></div>').attr('id',this.dom.admin.overlay).appendTo($('body'));
+
+        // to open the panel from the widget
+        var open_link = $('<a href="#">admin</a>').attr('id',this.dom.admin.open);
+        open_link.click(this.show);
+        widget.append(open_link);
+      },
+      show : function() {
+        $('#' + fb.i.dom.admin.panel).show();
+        $('#' + fb.i.dom.admin.overlay).show();
+      },
+      hide : function() {
+        $('#' + fb.i.dom.admin.panel).hide();
+        $('#' + fb.i.dom.admin.overlay).hide();
+      },
+    }
+    
+    this.main_window = $('<div></div>').attr('id',this.dom.widget).css({
       'width':'300px',
       'position':'absolute',
       'top':'15px',
@@ -16,6 +55,7 @@
       'border':'1px solid black',
       'padding':'5px'
     })
+    this.admin_panel.build(this.main_window);
     this.main_window.appendTo($('body'));
     
     if (typeof fb.Interface._initialized === "undefined") {}
@@ -27,7 +67,7 @@
   }
   
   fb.Interface.prototype.div = function() {
-  	return $('<div></div>');
+    return $('<div></div>');
   }
 
   fb.Interface.instantiated = false;
