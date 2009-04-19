4b82b6c189e79ea370b23d82f5b5d424bede964b
Andrew Bernstein <abernstein@berkeley.edu>
2009-04-19 14:47:43 -0700

    Adds basic test to make sure commenter invite emails get sent to the correct place

--- coreapp/test/unit/mailer_test.rb
+++ coreapp/test/unit/mailer_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class MailerTest < ActionMailer::TestCase
-  # replace this with your real tests
-  test "the truth" do
-    assert true
+
+  test 'Should send an email to the correct address' do
+	person = commenters(:one)
+    page = pages(:one)
+	invite = create_invite
+
+    mail = Mailer.create_commenter_invite(person, page)
+
+    assert_equal mail.to[0], person.email
   end
+
 end
