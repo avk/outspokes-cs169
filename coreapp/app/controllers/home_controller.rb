d4bc44a5fd80ea978b73799d25edb771645ea2f4
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-04-20 15:14:31 -0700

    re-enabled links on logged-in home page with admin url_tokens (temporary fix until Hannah's home page redesign)

--- coreapp/app/controllers/home_controller.rb
+++ coreapp/app/controllers/home_controller.rb
@@ -3,10 +3,8 @@ class HomeController < ApplicationController
   def index
     if logged_in?
       @sites = current_account.sites
-      @pages = current_account.pages
     else
-      @sites = Site.find(:all)
-      @pages = Page.find_all_by_site_id(nil)
+      @sites = []
     end
     respond_to do |format|
       format.html
--- coreapp/test/functional/home_controller_test.rb
+++ coreapp/test/functional/home_controller_test.rb
@@ -3,18 +3,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 class HomeControllerTest < ActionController::TestCase
   
   test "should get index" do
-	login_as nil
+    login_as nil
     get :index
     assert_response :success
-	assert assigns(:sites), Site.find(:all)
-	assert assigns(:pages), Page.find_all_by_site_id(nil)
   end
 
   test "should be able to log in" do
     login_as(:quentin)
-	get :index
+    get :index
     assert_response :success
     assert assigns(:sites), commenters(:quentin).sites
-	assert assigns(:pages), commenters(:quentin).pages
   end
 end
