4a35dc778a34684206d444d57e7e6c0600cafa34
sanxac <sanja9@gmail.com>
2009-04-21 11:52:41 -0700

    added help panel functionality

--- coreapp/app/js/fb.Interface.js
+++ coreapp/app/js/fb.Interface.js
@@ -19,6 +19,9 @@
         header  : 'topbar',
 		content : 'widget_content',
 		comments_header : 'comments_topbar',
+		help : 'help',
+		help_content: 'help_content',
+		toggle: 'toggle',
       },
       admin   : {
         panel   : 'outspokes_admin_panel',
@@ -60,26 +63,49 @@
     this.main_window = $('<div></div>').attr('id',this.dom.widget.wrapper);
 
 	this.topbar = $('<div></div>').attr('id',this.dom.widget.header);
+ 	var help_link = $('<a href="#">(?)</a>').attr('id',this.dom.widget.help);
+	var toggle_link = $('<a href="#">toggle</a>').attr('id',this.dom.widget.toggle);
 	this.topbar.append('<h1><a href="http://www.outspokes.com" target="_blank">outspokes y&#8216;all</a></h1>');
+	
+	help_link.click(function() {
+		var content = fb.i.widget_content;
+		var help = fb.i.help_content;
+		var widget = fb.i.main_window;
+		if (widget.height() == '20') {
+			widget.animate( { height:"220px" }, { duration:500 } );
+			help.toggleClass("hide");
+		} else {
+			content.toggle();
+			help.toggleClass("hide");
+		}
+	});
+	
+	this.topbar.append(help_link);
 
-	this.topbar.click(function() {
+	toggle_link.click(function() {
 		var content = fb.i.widget_content;
 		var widget = fb.i.main_window;
 		if (widget.height() == '20') {
 			widget.animate( { height:"220px" }, { duration:500 } );
-			content.toggle();
+			content.show();
 		} else {
 			widget.animate( { height:"20px" }, { duration:500 } );
-			content.toggle();
+			content.hide();
 		}
 	});
+	
+	this.topbar.append(toggle_link);
+	
 	this.main_window.append(this.topbar);
 
 	this.widget_content = $('<div></div>').attr('id',this.dom.widget.content);
+	this.help_content = $('<div>help yourself</div>').attr('id', this.dom.widget.help_content);
+	this.help_content.addClass("hide");
+	
 	this.chead = $('<div></div>').attr('id',this.dom.widget.comments_header);
 	var comment_span = $('<span>'+ fb.getProperties(fb.Feedback.all).length + ' comments</span>')
 	this.chead.append(comment_span);
-	this.chead.append('<select id="comments_filter"><option>newest</option><option>oldest</option><option>mine</option><option>targeted</option></select>');
+	this.chead.append('<select id="comments_filter"><option>newest</option><option>oldest</option><option>mine</option><option>targeted</option><option>consensus</option></select>');
 	
 	this.set_num_comments = function(num_comments) {
 	  comment_span.text(num_comments + ' comments');
@@ -87,6 +113,8 @@
 	
 	this.widget_content.append(this.chead);
 	this.main_window.append(this.widget_content);
+	
+	this.main_window.append(this.help_content);
 	
     this.admin_panel.build(this.topbar);
 
