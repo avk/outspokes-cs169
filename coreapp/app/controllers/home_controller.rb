cc68b74d6d924d0ab97965852bf2d847d158be98
Hannah Hu <hlhu@berkeley.edu>
2009-04-27 16:51:17 -0700

    Cleaned up HomeController and the Dashboard a bit. Also made some styling tweaks to the Dashboard

--- coreapp/app/controllers/home_controller.rb
+++ coreapp/app/controllers/home_controller.rb
@@ -3,20 +3,6 @@ class HomeController < ApplicationController
   def index
     if logged_in?
       redirect_to dashboard_account_url(current_account.id)
-    else
-      respond_to do |format|
-        format.html
-      end
-    end
-  end
-
-  def faq
-    render :action => :faq
-  end
-
-  def about
-    respond_to do |format|
-      format.html
     end
   end
 
