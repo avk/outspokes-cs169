1e1070dcf8afdd2737e09ac7323c4fc5e71202c5
Matthew Kocher <kocher@berkeley.edu>
2009-04-26 21:01:13 -0700

    workding in account controllers

--- coreapp/app/controllers/accounts_controller.rb
+++ coreapp/app/controllers/accounts_controller.rb
@@ -17,9 +17,9 @@ class AccountsController < ApplicationController
       # reset session
       self.current_account = @account # !! now logged in
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "Thanks for signing up!"
     else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      flash[:error]  = "Sorry, we could not create the account."
       render :action => 'new'
     end
   end
