7b3797f34eddcd6710344b9cf9c557ac7cd2f25f
sanxac <sanja9@gmail.com>
2009-04-26 20:00:17 -0700

    added help content

--- coreapp/app/js/fb.Interface.js
+++ coreapp/app/js/fb.Interface.js
@@ -180,7 +180,33 @@
     this.main_window.append(this.topbar);
 
     this.widget_content = $('<div></div>').attr('id',this.dom.widget.content);
-    this.help_content = $('<div>help yourself</div>').attr('id', this.dom.widget.help_content);
+    this.help_content = $('<div><h1>Outspokes Help</h1></div>').attr('id', this.dom.widget.help_content);
+    
+    var help_copy = "<h2>Minimized</h2>" +
+
+    "<p>Click on the center of the bar to expand Outspokes' feedback widget and start giving the owner of the page feedback!  " + 
+    "Also, you can click on our logo to go to outspokes.com and learn more about our service." +
+
+    "<h2>General</h2>" +
+
+    "<p>Click on a comment to minimize or maximize it.  If a comment is targeted, hover your mouse over it" + 
+    "to see what element of the page has been attached to the comment.</p>" +
+
+    "<p>To leave a comment, type in the text box on the right side of the widget, then click on the post button to the" + 
+    "bottom right of the text box to submit it.</p>" +
+    "<p>If you'd like to attach this comment to a portion of the page you're viewing, click the target icon on the top right of" + 
+    "the text box.</p>" +
+
+    "<p>To search the comments left on this page by keyword, type some text in the search bar on the top right of the widget panel," +
+    "and type enter." +
+    "To filter your result, use the drop-down bar above the list of comments to select how you'd want to filter the comments.</p>" +
+
+    "<p>Agreeing/disagreeing with a comment gives the person who requested feedback " +
+    "information about whether other commenters generally agree or disagree with a comment that another commenter has left.  " +
+    "Please note, once you have agreed or disagreed with a comment, you cannot change your vote.</p>";
+    
+    this.help_content.append(help_copy);
+    
     this.help_content.addClass("hide");
 
     this.chead = $('<div></div>').attr('id',this.dom.widget.comments_header);
