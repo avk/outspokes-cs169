d403c01afa27dcfe46751da56b6dce79e1a73018
James Duncan <james.t.duncan@gmail.com>
2009-05-01 15:59:31 -0700

    Added comments to the stuff in pre.js

--- coreapp/app/js/pre.js
+++ coreapp/app/js/pre.js
@@ -1,4 +1,7 @@
 (function() {
+  /* Forces any page with our widget code loaded to frame
+     bust out of the admin panel iframe, and the admin panel
+     iframe only.  */
   if (window.frameElement) {
     if (window.frameElement.id == 'outspokes_admin_panel_iframe') {
       top.location.replace(window.location.href);
@@ -6,6 +9,8 @@
     }
   }
 
+  /* Creates a function which returns a set value, the value
+     of which can only be set once, using .set().  */
   var _setter = function (default_value) {
     return (function() {
       var _isSet = false;
@@ -28,9 +33,14 @@
     })();
   };
 
+  /* A variable holding values that should not be accessible
+     outside of the widget code.  */
   var _fb = {};
+  // Boolean for whether or not the current user is authorized
   _fb.authorized = _setter(false);
+  // Boolean for whether or not the current user is the admin of the site
   _fb.admin = _setter(false);
+  // Holds the side_id, null until set.
   _fb.site_id = _setter(null);
 
 
