23de4dc3aefb4dc420342e9fc30aa8796f9e4a40
Matthew Kocher <kocher@berkeley.edu>
2009-05-09 23:59:39 -0700

    demo user_style

--- coreapp/app/controllers/widget/user_styles_controller.rb
+++ coreapp/app/controllers/widget/user_styles_controller.rb
@@ -1,5 +1,21 @@
 class Widget::UserStylesController < Widget::WidgetController
 
 
+  def css_for_user_style
+    @stored_json =   <<-eos
+    {
+                "html > div > div > span:eq(0)" : {
+                    "background-color" : "#333",
+                    "color" : "#efefef",
+                    "font-family" : "Times New Roman, Arial, Helvetica, sans-serif"
+                }
+            }
+            eos
+            
+    respond_to do |wants|
+      wants.css
+    end
+  end
+    
 
 end
