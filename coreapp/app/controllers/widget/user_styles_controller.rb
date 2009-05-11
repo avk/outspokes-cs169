450492fb07656ad64d2580a2bd30ff123e4ee3c8
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-05-10 17:38:58 -0700

    coreapp: removing old CSS fetch code in Widget::UserStylesController

--- coreapp/app/controllers/widget/user_styles_controller.rb
+++ coreapp/app/controllers/widget/user_styles_controller.rb
@@ -55,22 +55,5 @@ class Widget::UserStylesController < Widget::WidgetController
       end
     end
   end
-  
-  # def css_for_user_style
-    # @stored_json =   <<-eos
-    # {
-    #             "html > div > div > span:eq(0)" : {
-    #                 "background-color" : "#333",
-    #                 "color" : "#efefef",
-    #                 "font-family" : "Times New Roman, Arial, Helvetica, sans-serif"
-    #             }
-    #         }
-    #         eos
-  #           
-  #   respond_to do |wants|
-  #     wants.css
-  #   end
-  # end
-    
 
 end
