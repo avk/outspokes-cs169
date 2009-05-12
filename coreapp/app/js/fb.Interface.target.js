f7f28ab06ce0f55ba69431c2a4a3990e60d1dac5
sanxac <sanja9@gmail.com>
2009-05-12 12:32:10 -0700

    styling target list

--- coreapp/app/js/fb.Interface.target.js
+++ coreapp/app/js/fb.Interface.target.js
@@ -17,15 +17,17 @@
     }; // if changed, also change in clearAll()
     
     this.target_header = $('<div></div>').attr('id', 'outspokes_target_header');
-    this.target_header.append($('<h1>Targeting</h1>'));
     var target_button = $('<img class="outspokes_target_button" src="' + fb.env.target_address + '" />');
+
+
     target_button.click(function() {
       fb.Target.pick(function(target) { // callback
         
       });
     });
     this.target_header.append(target_button);
-    this.target_header.append($('<div style="clear:both;"></div>'));
+    this.target_header.append($('<span>Targeting</span>'));
+   // this.target_header.append($('<div style="clear:both;"></div>'));
     
     
     this.target_list = $('<ul></ul>').attr('id','outspokes_target_list');
@@ -109,8 +111,9 @@
     };
     
     this.startOver();
+    
     self.user_style.your_targets.append(this.target_header);
-    self.user_style.your_targets.append(this.target_list);
+    self.user_style.your_targets.append($('<div id="outspokes_target_list_wrap"></div>').append(this.target_list));
     self.user_style.your_targets.append(this.target_footer);
     
   };
