ecfdab78b07e5f128a16488155298398fcdd32cb
Matthew Kocher <kocher@berkeley.edu>
2009-05-11 19:47:06 -0700

    gem require

--- coreapp/config/environment.rb
+++ coreapp/config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "json"
   config.gem "ruby-debug"
   config.gem "haml"
+  config.gem "juggernaut"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
