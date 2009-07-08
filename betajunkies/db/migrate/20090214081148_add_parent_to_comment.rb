ae28987a0b07f56d1b11d7749265a4fa3673a8a0
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-07-07 23:11:24 -0700

    removing legacy CS 169 subdirectories and documents

--- betajunkies/db/migrate/20090214081148_add_parent_to_comment.rb
+++
@@ -1,9 +0,0 @@
-class AddParentToComment < ActiveRecord::Migration
-  def self.up
-    add_column :comments, :parent, :integer
-  end
-
-  def self.down
-    remove_column :comments, :parent
-  end
-end
--- runthisbyus/app/models/idea.rb
+++
@@ -1,8 +0,0 @@
-class Idea < ActiveRecord::Base
-
-  has_many :comments
-
-  validates_presence_of :name
-  validates_presence_of :body
-
-end
