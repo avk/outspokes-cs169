a2c486f6f9a3a66fd2234e1e2ecc9d4c42b06e49
Jerry Cheung <jollyjerry@gmail.com>
2009-07-11 15:18:34 -0700

    * use JSON.stringify instead of make_json for better compatibility * start tests for fb.JSON

--- coreapp/app/controllers/selenium_controller.rb
+++ coreapp/app/controllers/selenium_controller.rb
@@ -1,6 +1,15 @@
 class SeleniumController < ApplicationController
+  before_filter :set_fb_hash
+
   def test_fb_hash
+  end
+
+  def test_fb_json
+    @file = File.join(RAILS_ROOT, 'app', 'js', 'fb.json.js')
+  end
+
+  protected
+  def set_fb_hash
     @fb_hash = "fb_g3n3rat3d123"
-    @file = File.join(RAILS_ROOT, 'app', 'js', 'fb_hash.js.erb')
   end
 end
--- coreapp/app/helpers/selenium_helper.rb
+++ coreapp/app/helpers/selenium_helper.rb
@@ -1,2 +1,6 @@
 module SeleniumHelper
+  def render_fb_hash(fb_hash_identifier)
+    @fb_hash = fb_hash_identifier
+    render :file => File.join(RAILS_ROOT, 'app', 'js', 'fb_hash.js.erb')
+  end
 end
