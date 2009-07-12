04485276c80ddf00d11f26661b9cb031b51e9e58
Jerry Cheung <jollyjerry@gmail.com>
2009-07-11 17:08:09 -0700

    * remove duplicate error validation on Commenter/Account

--- coreapp/app/models/account.rb
+++ coreapp/app/models/account.rb
@@ -10,13 +10,9 @@ class Account < Commenter
   
   belongs_to :commenter
 
+  # TODO: do we plan to use 'name' field at all?
   validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
   validates_length_of       :name,     :maximum => 100
-
-  validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
-  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
   after_create :deliver_welcome_email
 
