449ccc925935fa0840943a8a816d690ab7b0c4c1
Jerry Cheung <jollyjerry@gmail.com>
2009-07-19 18:22:41 -0700

    * removing duplicated and unused regexes for email validation from Commenter model

--- coreapp/app/models/commenter.rb
+++ coreapp/app/models/commenter.rb
@@ -7,13 +7,6 @@ class Commenter < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :user_styles, :dependent => :destroy
 
-  #stolen from the restful_authentication plugin
-  @@email_name_regex  = '[\w\.%\+\-]+'.freeze
-  @@domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
-  @@domain_tld_regex  = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
-  @@email_regex       = /\A#{@@email_name_regex}@#{@@domain_head_regex}#{@@domain_tld_regex}\z/i
-  @@bad_email_message = "should look like an email address.".freeze
-
   validates_presence_of :email, :allow_blank => false
   validates_length_of   :email, :within => 6..100 #r@a.wk
   validates_format_of   :email, :with => Authentication.email_regex, :message => Authentication.bad_email_message
