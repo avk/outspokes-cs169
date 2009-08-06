8eafa9638fa7ba1f8fecf3b5d5f6f3a0078ced3a
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-08-06 16:04:45 -0700

    Revert "invites now take time into account to generate more unique url tokens"
    This reverts commit 6001bfaffb6682a0b8efd32156437d3415d880e8.
    
    Conflicts:
    
    	coreapp/test/unit/invite_test.rb

--- coreapp/app/models/invite.rb
+++ coreapp/app/models/invite.rb
@@ -22,7 +22,7 @@ class Invite < ActiveRecord::Base
   def generate_url_token
     # a long random string is a good salt: some random text's md5
     salt = '99fedcdf9e17b193a2eecb9e26a5e2fa'
-    self.url_token = Digest::MD5::hexdigest(self.commenter.email + salt + self.page.url + Time.now.to_s)
+    self.url_token = Digest::MD5::hexdigest(self.commenter.email + salt + self.page.url)
   end
 
 end
