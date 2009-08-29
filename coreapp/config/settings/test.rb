cf48934b4be924c83690e10e66b0859289c4c5bf
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-08-29 15:09:29 -0700

    first Selenium tests (all user authentication) for widget on demo site

--- coreapp/config/settings/test.rb
+++ coreapp/config/settings/test.rb
@@ -4,5 +4,5 @@ SimpleConfig.for :application do
   set :url, "http://#{domain}"
   set :selenium_demo_port, "2009"
   set :selenium_demo_domain, "localhost:#{selenium_demo_port}"
-  set :selenium_demo_url, "http://#{selenium_demo_domain}"
+  set :selenium_demo_url, "http://#{selenium_demo_domain}/demo"
 end
